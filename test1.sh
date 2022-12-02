1b7c95277515541f53e4fe2777ade0b4d04ac01f
goblin <goblin@ubuntu.com>
2022-12-02 13:35:07 -0800

    add sudo check

--- test1.sh
+++ test1.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+#check if root
+if [ "$EUID" -ne 0 ]; then
+  echo "please run as root"
+  exit 1
+fi
+
 # basic updates
-sudo apt update -y && sudo apt upgrade -y
+apt update -y && sudo apt upgrade -y
 if ! [ $? -eq 0 ]; then
   echo 'something went wrong during apt-update'
   echo 'exiting the script'
   exit 1
 fi
 
-sudo apt install snapd -y
-sudo apt install curl -y
+apt install snapd -y
+apt install curl -y
 
 
 # code editors
+# code editors
+snap install code --classic
+apt install vim -y
+
