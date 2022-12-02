cc0192eb7835d8e10291794f25cbcb7730967d34
goblin <goblin@ubuntu.com>
2022-12-02 14:10:21 -0800

    custom output to show installed packeages as the script run

--- test1.sh
+++ test1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Usage: ubuntu-setup
+# This script will install basic software on your ubuntu using apt and snap
+
 
 #check if root
 if [ "$EUID" -ne 0 ]; then
@@ -7,19 +10,19 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # basic updates
-apt update -y && sudo apt upgrade -y
+apt update -y 1>/dev/null 2>&1 && sudo apt upgrade -y 1>/dev/null 2>&1
 if ! [ $? -eq 0 ]; then
   echo 'something went wrong during apt-update'
   echo 'exiting the script'
   exit 1
 fi
 
-apt install snapd -y
-apt install curl -y
+apt install snapd -y 1>/dev/null 2>&1 && echo 'snap was installed' || echo 'error installid snap'
+apt install curl -y 1>/dev/null 2>&1 && echo 'discord was installed' || echo 'erorr installing curl'
 
 
 # code editors
 # code editors
-snap install code --classic
-apt install vim -y
+snap install code --classic 1>/dev/null 2>&1 && echo 'visual code was installed' || echo 'error installing visual code'
+apt install vim -y 1>/dev/null 2>&1 && echo 'vim was installed' || echo 'eroor installing vim'
 
