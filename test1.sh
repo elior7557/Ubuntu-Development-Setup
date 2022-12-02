03a9cc4dce35aa988873c21a52d90edb4f1ec14e
goblin <goblin@ubuntu.com>
2022-12-02 15:23:25 -0800

    echo lines and utilities install added

--- test1.sh
+++ test1.sh
@@ -10,6 +10,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # basic updates
+echo "Starting The Script By Runnint apt update && Upgrade"
 apt update -y 1>/dev/null 2>&1 && sudo apt upgrade -y 1>/dev/null 2>&1
 if ! [ $? -eq 0 ]; then
   echo 'something went wrong during apt-update'
@@ -17,12 +18,19 @@ if ! [ $? -eq 0 ]; then
   exit 1
 fi
 
+echo "Updates run successfuly staring to install some application: "
+
 apt install snapd -y 1>/dev/null 2>&1 && echo 'snap was installed' || echo 'error installid snap'
-apt install curl -y 1>/dev/null 2>&1 && echo 'discord was installed' || echo 'erorr installing curl'
+apt install curl -y 1>/dev/null 2>&1 && echo 'curl was installed' || echo 'error installing curl'
 
-
-# code editors
 # code editors
 snap install code --classic 1>/dev/null 2>&1 && echo 'visual code was installed' || echo 'error installing visual code'
-apt install vim -y 1>/dev/null 2>&1 && echo 'vim was installed' || echo 'eroor installing vim'
+apt install vim -y 1>/dev/null 2>&1 && echo 'vim was installed' || echo 'error installing vim'
+
+# utilities
+snap install discord 1>/dev/null 2>&1 && echo 'Discord was installed' || echo 'error installing Discord'
+snap install slack 1>/dev/null 2>&1 && echo 'slack was installed' || echo 'error installing slack'
+
+#DevOps Setup
+#Docker
 
