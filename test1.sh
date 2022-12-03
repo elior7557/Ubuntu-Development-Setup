2c7202254a74ca52be865ee27573f407657744c4
goblin <goblin@ubuntu.com>
2022-12-02 16:08:40 -0800

    added T flag to test.sh

--- test1.sh
+++ test1.sh
@@ -9,6 +9,8 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+
+
 # basic updates
 echo "Starting The Script By Runnint apt update && Upgrade"
 apt update -y 1>/dev/null 2>&1 && sudo apt upgrade -y 1>/dev/null 2>&1
@@ -20,17 +22,36 @@ fi
 
 echo "Updates run successfuly staring to install some application: "
 
-apt install snapd -y 1>/dev/null 2>&1 && echo 'snap was installed' || echo 'error installid snap'
-apt install curl -y 1>/dev/null 2>&1 && echo 'curl was installed' || echo 'error installing curl'
+#Skip some installs for testin purposes
+if [ $1 == 'T' ]; then
+  echo 'Testin mode is on'
+else
+  apt install snapd -y 1>/dev/null 2>&1 && echo 'snap was installed' || echo 'error installig snap'
+  apt install curl -y 1>/dev/null 2>&1 && echo 'curl was installed' || echo 'error installing curl'
 
-# code editors
-snap install code --classic 1>/dev/null 2>&1 && echo 'visual code was installed' || echo 'error installing visual code'
-apt install vim -y 1>/dev/null 2>&1 && echo 'vim was installed' || echo 'error installing vim'
+  # code editors
+  snap install code --classic 1>/dev/null 2>&1 && echo 'visual code was installed' || echo 'error installing visual code'
+  apt install vim -y 1>/dev/null 2>&1 && echo 'vim was installed' || echo 'error installing vim'
 
-# utilities
-snap install discord 1>/dev/null 2>&1 && echo 'Discord was installed' || echo 'error installing Discord'
-snap install slack 1>/dev/null 2>&1 && echo 'slack was installed' || echo 'error installing slack'
-
+  # utilities
+  snap install discord 1>/dev/null 2>&1 && echo 'Discord was installed' || echo 'error installing Discord'
+  snap install slack 1>/dev/null 2>&1 && echo 'slack was installed' || echo 'error installing slack'
+fi
 #DevOps Setup
-#Docker
 
+#Docker
+echo 'Installing Docker'
+curl --silent -o /dev/null https://get.docker.com/ > docker.sh && chmod u+x docker.sh
+./docker.sh 1> /dev/null 2>&1 && echo 'docker was installed' || echo 'error installing docker'
+chmod 777 /var/run/docker.sock
+rm ./docker.sh
+
+#Docker Desktop
+echo 'Installing Docker Desktop'
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.14.1-amd64.deb?utm_source=docker -q --show-progress -O docker-desktop.deb && echo 'deb file installed'
+sudo apt-get install ./docker-desktop.deb -y 1> /dev/null 2>&1 && echo 'Docker Desktop was installed' || echo 'error installing docker desktop'
+rm docker-desktop.deb
+
+#Updating Bashrc File
+echo " Update bashrc file"
+curl https://raw.githubusercontent.com/linuxacademy/content-intro-to-python-development/master/helpers/bashrc -o ~/.bashrc
